99a78ee19a0557a1d744b05721d4098d03988b0c
cqjxby02 <38235494+cqjxby02@users.noreply.github.com>
2018-07-30 09:22:38 +0800

    1-30

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	 /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '192.168.0.118', // 服务器地址
+    'DB_HOST'               =>  '192.168.0.126', // 服务器地址
     'DB_NAME'               =>  'zidao',          // 数据库名
     'DB_USER'               =>  'zidao',      // 用户名
     'DB_PWD'                =>  'zidao',          // 密码
@@ -17,4 +17,4 @@ return array(
     'DB_RW_SEPARATE'        =>  false,       // 数据库读写是否分离 主从式有效
     'DB_MASTER_NUM'         =>  1, // 读写分离后 主服务器数量
     'DB_SLAVE_NO'           =>  '', // 指定从服务器序号
-);
+); 
--- Public/Admin/data/admin_config.php
+++ Public/Admin/data/admin_config.php
@@ -2,7 +2,7 @@
 return array(
     //first{
     //是否开启日志记录
-    'if_write_in'=>'1',
+    'if_write_in'=>'0',
     //是否开启APP
     'Software_power'=>'on',
     //}
